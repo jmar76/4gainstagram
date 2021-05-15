5a7625eb28f50644143153d046f4831ec0ce33eb
jmar76 <jmaguero.ing@gmail.com>
2021-05-15 16:21:54 +0000

    advanced

--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
@@ -7,9 +8,10 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
+db=SQLAlchemy()
 
-class Users(Base):
-    __tablename__ = 'users'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     ID = Column(Integer, primary_key=True)
@@ -19,21 +21,29 @@ class Users(Base):
     age = Column(String(250), nullable=False)
     image = Column(String(250), nullable=False)
 
-class Followers(Base):
-    __tablename__ = 'followers'
+    posts = db.relationship("post", uselist=False, back_populates="user")
+    comments = db.relationship("comment", uselist=False, back_populates="user")
+
+class Follower(Base):
+    __tablename__ = 'follower'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     user_from_id = Column(integer,nullable=False)
     user_to_id = Column(integer,nullable=False)
 
 class Post(Base):
-    __tablename__ = 'posts'
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     ID = Column(Integer, primary_key=True)
     user_id =  Column(integer,nullable=False)
     image =  Column(String(250),nullable=False)
     text =  Column(String(250),nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship("user", back_populates="post")
+
+    comments = db.relationship("comment", uselist=False, back_populates="posts")
 
 class Comment(Base):
     __tablename__ = 'comment'
@@ -43,6 +53,12 @@ class Comment(Base):
     author_comment_id = Column(integer, nullable=False)
     post_id = Column(integer, nullable=False)
     text =  Column(String(250), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship("user", back_populates="comments")
+
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
+    post = db.relationship("post", back_populates="comments")
 
 
 
